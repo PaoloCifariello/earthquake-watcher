26d44586a872dbd9ad5ca6c530044c720f8140cd
Paolo Cifariello <paolocifa@gmail.com>
2016-06-30 18:18:51 +0200

    different marker for selected eq, reduced proxy data

--- js/Fetcher.js
+++ js/Fetcher.js
@@ -3,23 +3,20 @@ class Fetcher {
         this._proxy = proxy || null;
         this._url = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson";
     }
-    
     fetchData(options) {
         let promiseToReturn = null;
-        
         if (this._proxy) {
             promiseToReturn = new Promise((resolve, reject) => {
                 resolve(this._proxy.getData());
             });
-        } else {
+        }
+        else {
             let url = this._url;
             $.each(options, (key, value) => {
                 url += '&' + key + '=' + value;
             });
-
-            promiseToReturn = $.getJSON(url);    
+            promiseToReturn = $.getJSON(url);
         }
-        
         return promiseToReturn;
     }
 }
--- js/main.js
+++ js/main.js
@@ -2,32 +2,37 @@ var currentData = [];
 var markers = [];
 
 function initMap() {
-    let zoom = 2,
-        center = {lat: 41.850, lng: -87.650};
-    
+    let zoom = 2
+        , center = {
+            lat: 41.850
+            , lng: -87.650
+        };
     window.map = new Map(zoom, center);
     map.initializeMap();
-    
     map.on('bounds_changed', () => {
         let visibleEarthquakes = map.getVisibleEarthquakes();
         $('#list-panel').empty();
-        
         $.each(visibleEarthquakes, (i, earthquake) => {
-            $('#list-panel').append(getListElement(earthquake));
+            let listElement = getListElement(earthquake);
+            listElement.click(() => {
+                let coords = earthquake.geometry.coordinates
+                    , position = new google.maps.LatLng(coords[1], coords[0]);
+                map._selectedFeature = earthquake;
+                map.refresh();
+            });
+            $('#list-panel').append(listElement);
         });
     });
-    
     // Set mouseover event for each feature.
-//    map.data.addListener('mouseover', function(event) {
-//        console.log(event);
-//    });
-//    
-//    fetchEarthquakes();
+    //    map.data.addListener('mouseover', function(event) {
+    //        console.log(event);
+    //    });
+    //    
+    //    fetchEarthquakes();
     let fetcher = new Fetcher(new Proxy());
-    
     fetcher.fetchData({
-        starttime: '2016-01-01',
-        endtime: '2016-01-02'
+        starttime: '2016-01-01'
+        , endtime: '2016-01-02'
     }).then((data) => {
         /* GeoJSON object, type: FeatureCollection */
         map.setData(data);
@@ -35,11 +40,9 @@ function initMap() {
 }
 
 function getListElement(earthquake) {
-    return $('<a href="#" class="list-group-item"> ' + earthquake.id + '</a>');
+    return $('<a href="#" class="list-group-item"> ' + earthquake.id + '<br>Magnitude ' + earthquake.properties.mag + '</a>');
 }
-
-
-$(function() {
+$(function () {
     $('div#radio-options input').change(() => {
         let visualizationType = $('div#radio-options input:checked').val();
         map.setVisualizationType(visualizationType);
