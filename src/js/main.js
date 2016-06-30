31f89e173dacd86246002bade4cf3343dcca839a
Paolo Cifariello <paolocifa@gmail.com>
2016-06-30 13:43:27 +0200

    removed unused code

--- src/js/main.js
+++ src/js/main.js
@@ -31,39 +31,3 @@ $(function() {
         map.setVisualizationType(visualizationType);
     });
 });
-
-function fetchEarthquakes() {
-    var dataPromise = $.getJSON( "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-01-02");
-    dataPromise.then(function(data) {
-//                var heatmapData = [];
-//    for (var i = 0; i < results.features.length; i++) {
-//      var coords = results.features[i].geometry.coordinates;
-//      var latLng = new google.maps.LatLng(coords[1], coords[0]);
-//      var magnitude = results.features[i].properties.mag;
-//      var weightedLoc = {
-//        location: latLng,
-//        weight: Math.pow(2, magnitude)
-//      };
-//      heatmapData.push(weightedLoc);
-//    }
-//    var heatmap = new google.maps.visualization.HeatmapLayer({
-//      data: heatmapData,
-//      dissipating: false,
-//      map: map
-//    });
-    });
-}
-
-function addEarthquakeToMap(earthquake) {
-
-//    var cityCircle = new google.maps.Circle({
-//      strokeColor: '#FF0000',
-//      strokeOpacity: 0.8,
-//      strokeWeight: 2,
-//      fillColor: '#FF0000',
-//      fillOpacity: 0.35,
-//      map: map,
-//      center: citymap[city].center,
-//      radius: Math.sqrt(citymap[city].population) * 100
-//    });
-}
